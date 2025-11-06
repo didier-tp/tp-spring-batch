1c3f14af111afe6602c70087fe2c1b8f9da2bf2f
Didier_Defrance <didier@d-defrance.fr>
2025-11-06 15:00:31 +0100

    s

--- tp/2025/marseille/tpSpringBatch/src/main/java/tp/tpSpringBatch/processor/SimpleUppercaseProductProcessor.java
+++ tp/2025/marseille/tpSpringBatch/src/main/java/tp/tpSpringBatch/processor/SimpleUppercaseProductProcessor.java
@@ -1,14 +1,25 @@
 package tp.tpSpringBatch.processor;
 
+import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.item.ItemProcessor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import tp.tpSpringBatch.model.Product;
 
 @Component
+@JobScope
 public class SimpleUppercaseProductProcessor implements ItemProcessor<Product,Product> {
+
+    @Value("#{jobParameters['enableUpperCase']}")
+    private Boolean enableUpperCase=true;
+
     public Product process(Product p) throws Exception {
+
+        String mainCategory =enableUpperCase?p.getMain_category().toUpperCase():p.getMain_category();
+        //String mainCategory = p.getMain_category().toUpperCase();
+
         Product product=new Product(p.getId(),
-                                   p.getMain_category().toUpperCase() ,
+                                   mainCategory ,
                                    p.getLabel(),
                                    p.getPrice(),
                                    p.getTime_stamp(),
--- tp/2025/marseille/tpSpringBatch/src/main/script/launchCsvToXmlBatch.bat
+++ tp/2025/marseille/tpSpringBatch/src/main/script/launchCsvToXmlBatch.bat
@@ -3,10 +3,11 @@ set JAVA_HOME=C:\Program Files\Java\jdk-21
 set PATH=%JAVA_HOME%/bin;%PATH%
 REM java -version
 set inputFilePath=data/input/csv/products.csv
-set outputFilePath=data/output/xml/products_3.xml
+set outputFilePath=data/output/xml/products_4.xml
+set enableUpperCase=false
 set JOB_NAME=fromCsvToXmlJob
 set MAIN_CLASS=tp.tpSpringBatch.TpSpringBatchApplication
 set CP=target/tpSpringBatch.jar
 cd /d %~dp0/%PROJECT_ROOT%
-java -DinputFilePath=%inputFilePath% -DoutputFilePath=%outputFilePath% -jar %CP% %JOB_NAME%
+java -DinputFilePath=%inputFilePath% -DoutputFilePath=%outputFilePath% -DenableUpperCase=%enableUpperCase% -jar %CP% %JOB_NAME%
 pause
